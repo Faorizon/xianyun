7de809ec3746443c6e0a79f751457acea1c7cfc5
Faorizon <53572881+Faorizon@users.noreply.github.com>
2019-10-11 21:29:15 +0800

    首页轮播图

---
+++ day1.js
@@ -0,0 +1,135 @@
+var arr = [1,2,3,4];
+
+var arr1=[];
+arr.forEach(v => {
+    arr1.push(v+1)
+});
+
+var arr2=arr.map(v=>{
+    return v+1
+})
+
+var obj1={a:1,b:2}
+var obj2={c:3,d:4}
+
+var obj={...obj1,...obj2}
+var obj3=Object.assign(obj1,obj2)
+
+var obj4={a:1,b:2,c:3,d:4}
+
+// 删除单个
+// delete obj4.a
+// console.log(obj4)
+// var {a,b,...ddd}=obj4
+// console.log(ddd)
+
+//对象循环
+var obj5={a:1,b:2,c:3,d:4}
+var newObj={};
+for(var key in obj5){
+    newObj[key]=obj5[key]+1
+}
+// console.log(newObj)
+
+var newObj1={}
+Object.keys(obj5).forEach(v=>{
+    newObj1[v]=obj5[v]+1
+})
+// console.log(newObj1,Object.keys(obj5))
+// var pro = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         var res = 60;
+
+//         if(res >= 60){
+//             resolve()
+//         }else{
+//             reject();
+//         }
+//     }, 2000)
+// })
+
+// // pro当做axios
+// pro.then(() => {
+//     console.log("加鸡腿")
+// }).catch(() => {
+//     console.log("混合双打")
+// })
+
+function test(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            var res=60;
+            if(res>=60){
+                resolve()
+            }else{
+                reject()
+            }
+        },2000)
+    })
+}
+test().then(()=>{
+    console.log("加鸡腿")
+}).catch(()=>{
+    console.log("混合双打")
+})
+
+// 数学
+function shuxue(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            var res = 60;
+
+            if(res >= 60){
+                resolve()
+            }else{
+                reject();
+            }
+        }, 2000)
+    })
+}
+
+// 英语
+function yingyu(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            var res = 60;
+
+            if(res >= 60){
+                resolve()
+            }else{
+                reject();
+            }
+        }, 2000)
+    })
+}
+// yuwen
+function yuwen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            var res = 60;
+
+            if(res >= 60){
+                resolve()
+            }else{
+                reject();
+            }
+        }, 2000)
+    })
+}
+
+var test = async function(){
+    // promise对象的then函数返回一个promise
+    await yuwen().then(() => {
+        console.log("加鸡腿");
+    });
+
+    await shuxue().then(() => {
+        console.log("加鸡腿,加鸡翅");
+    })
+
+    await yingyu().then(() => {
+        console.log("加鸡腿,加鸡翅,加鸡头"); 
+    })
+}
+
+test();
---
+++ pages/tab.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="tabItem">
+            <span 
+            v-for="(item,index) in tab" 
+            :key="index" 
+            @click="handleCurrent(index)"
+            :class="{active:index===current}"
+            >{{item}}</span>
+        </div>
+        <div class="content">
+            <div class="news" v-if="current===0">
+                韩国检察官地位高、权力大，就容易出现贪污腐败、政商勾结。文在寅曾强调，在一些涉及社会特权阶层案件中，韩国检察厅和警察厅等调查机构涉嫌故意疏漏调查，甚至主动妨碍调查，隐瞒真相，庇护涉案人。
+            </div>
+            <div class="amusement" v-if="current===1">
+                文在寅曾强调，在一些涉及社会特权阶层案件中，韩国检察厅和警察厅等调查机构涉嫌故意疏漏调查，甚至主动妨碍调查，隐瞒真相，庇护涉案人。
+            </div>
+            <div class="physical" v-if="current===2">
+                韩国检察厅和警察厅等调查机构涉嫌故意疏漏调查，甚至主动妨碍调查，隐瞒真相，庇护涉案人。
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0,
+            tab:["新闻","娱乐","体育"]
+        }
+    },
+    methods:{
+        handleCurrent(index){
+            this.current=index
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .tabItem{
+        margin-bottom: 20px;
+        span{
+            cursor:pointer;
+            border:1px #ddd solid;
+            padding:10px;
+        }
+        .active{
+            color:red;
+            border-color: red;
+        }
+    }
+</style>
