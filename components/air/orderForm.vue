b1875ca8469c57ac856d178e5cd3c0519f28139a
Faorizon <53572881+Faorizon@users.noreply.github.com>
2019-10-18 16:09:48 +0800

    提价数据成功

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -159,7 +159,11 @@ export default {
             this.$axios({
                 url:'/airorders',
                 method:'post',
-                data
+                data,
+                headers:{
+                    //这是jwt标准的token
+                    Authorization:`Bearer ${this.$store.state.user.userInfo.token}`
+                }
             }).then(res=>{
                 console.log(res)
             })
