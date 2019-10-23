c2343f8a421e645bb44997fcebc3a81bdf04b1b0
Faorizon <53572881+Faorizon@users.noreply.github.com>
2019-10-23 10:47:38 +0800

    旅游攻略首页布局基本完成

---
+++ components/post/postList.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="post-list">
+        <ul>
+            <li>
+                <el-row type="flex">
+                    <div class="post-left">
+                        <a href="#">
+                            <img src="1.png" alt="" class="post-img">
+                        </a>
+                    </div>
+                    <div class="post-right">
+                        <div class="post-title">
+                            <a href="#">
+                                啊啊啊啊啊
+                            </a>
+                        </div>
+                        <div class="post-content">
+                            <a href="#">
+                                这是文章内容
+                            </a>
+                        </div>
+                        <el-row type="flex" align="middle" class="post-info" justify="space-between">
+                            <div>   
+                                <span class="location"><i class="iconfont el-icon-location-outline"></i>北京市</span>
+                                <span>by</span>
+                                <span class="userInfo">
+                                    <a href="#">
+                                        <img src="1.png" alt="" class="userImg">
+                                        <span class="userName">地球发动机</span>
+                                    </a>
+                                </span>
+                                <span class="viewCount"><i class="iconfont el-icon-view"></i>63</span>
+                            </div>
+                            <span class="like">3点赞</span>
+                        </el-row>
+                    </div> 
+                </el-row>
+                
+            </li>
+        </ul>
+        <div class="three">
+            <div class="post-title">
+                塞班贵？一定是你打开的方式不对！6000块玩转塞班
+            </div>
+            <div class="post-content">
+                大家对塞班岛总存在着这样的误解，知道它是美属地盘，就理所当然地觉得这里的花费一定很高，花费高有高的玩法，那如果只有6000块的预算呢？要怎么玩？关于旅行这件事，我们要让钱花得更有道理，收下这份攻略，带你6000块花式玩转塞班。图：塞班岛。 by第5季旅游一、怎样用6000块玩转塞班？大多数出境游客人不做预算或最
+            </div>
+            <el-row type="flex" justify="space-between" class="post-pic" :gutter="20">
+                <el-col :span="8">
+                    <img src="1.png" alt="" class="post-img">
+                </el-col>
+            </el-row>
+            <el-row type="flex" align="middle" class="post-info" justify="space-between" style="width:100%;">
+                <div>   
+                    <span class="location"><i class="iconfont el-icon-location-outline"></i>北京市</span>
+                    <span>by</span>
+                    <span class="userInfo">
+                        <a href="#">
+                            <img src="1.png" alt="" class="userImg">
+                            <span class="userName">地球发动机</span>
+                        </a>
+                    </span>
+                    <span class="viewCount"><i class="iconfont el-icon-view"></i>63</span>
+                </div>
+                <span class="like">3点赞</span>
+            </el-row>
+        </div>
+        <div class="block">
+        <el-pagination
+            :current-page="1"
+            :page-sizes="[2, 3, 5, 10]"
+            :page-size="5"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            total:100,
+        }
+    },
+    methods:{
+        handleSizeChange(){
+            console.log("size变了")
+        },
+        handleCurrentChange(){
+            console.log("当前页变了")
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.post-list{
+    .post-img{
+        width: 220px;
+        border:1px solid #ccc;
+        object-fit: cover;
+    }
+    .post-info{
+        padding-top: 13px;
+        color:#999;
+        font-size: 12px;
+        i{
+            font-size: 12px;
+        }
+        .location{
+            margin-right: 10px;
+        }
+        .userInfo{
+            margin:0 10px;
+            .userImg{
+                width:16px;
+                height:16px;
+                vertical-align: middle;
+                border:2px #fff solid;
+                border-radius:50px;
+            }
+            .userName{
+                color:#ffa500;
+            }
+        }
+        .like{
+            float: right;
+            color:#ffa500;
+            font-size: 16px;
+        }
+    }
+    .post-title{
+        font-size: 18px;
+        line-height: 24px;
+        font-weight: 400;
+        cursor: pointer;
+        &:hover{
+            color:#ffa500;
+        }
+    }
+    .post-content{
+        height: 65px;
+        margin:15px 0;
+        font-size: 14px;
+        color:#666;
+        cursor: pointer;
+    }
+    li{
+        width:700px;
+        margin-top: 15px;
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 15px;
+        .post-left{
+            width:220px;
+        }
+        .post-right{
+            margin-left: 10px;
+            width: 470px;  
+        }
+    }
+    .three{
+        width: 700px;
+        margin-top: 15px;
+        border-bottom: 1px solid #ccc;
+        padding-bottom: 15px;
+        .post-img{
+            border:none
+        }
+    }
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,216 @@
 <template>
-    <div>
-        旅游攻略模块首页
+    <div class="box">
+        <el-row type="flex" class="row-bg">
+            <div class="left">
+                <el-col :span="6">
+                    <div class="grid-content bg-purple menu">
+                        <ul>
+                            <li class="menu-item">热门城市 <span class="iconfont el-icon-arrow-right"></span></li>
+                            <li class="menu-item">推荐城市 <span class="iconfont el-icon-arrow-right"></span></li>
+                            <li class="menu-item">奔向海岛 <span class="iconfont el-icon-arrow-right"></span></li>
+                            <li class="menu-item">主题推荐 <span class="iconfont el-icon-arrow-right"></span></li>
+                        </ul>
+                        <div class="recommend">
+                                <div class="recommendItem">
+                                    <span class="ranking">1</span>
+                                    <span class="city">北京</span>
+                                    <span class="describe">世界著名古都和现代化国际城市</span>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="wishful">
+                        <h4>推荐城市</h4>
+                        <img src="1.png" alt="">
+                    </div>
+                </el-col>
+            </div>
+            <div class="right">   
+                <el-col :span="18">
+                    <div class="grid-content bg-purple-light">
+                        <div class="search">
+                            <input type="text" placeholder="请输入想去的地方,比如：广州" v-model="travelSearch" @keydown.enter="handleSearch">
+                            <i class="iconfont el-icon-search" @click="handleSearch"></i>
+                        </div>
+                        <div class="searchRecommend">
+                            推荐：
+                            <a href="#">北京</a>
+                            <a href="#">上海</a>
+                            <a href="#">广州</a>
+                        </div>
+                        <el-row type="flex" justify="space-between" align="middle" class="recommend-title">
+                            <h4>推荐攻略</h4>
+                            <el-button type="primary" icon="el-icon-edit" @click.native="$router.push('/post/create')">
+                                写游记
+                            </el-button>
+                        </el-row>
+                    </div>
+                    <PostList/>
+                </el-col>
+            </div>
+            
+        </el-row>
     </div>
 </template>
 
 <script>
+import PostList from "@/components/post/postList"
 export default {
-
+    components:{
+        PostList
+    },
+    data(){
+        return{
+            travelSearch:""
+        }
+    },
+    methods:{
+        handleSearch(){
+            console.log(123)
+        }
+    },
+    mounted(){
+        
+    }
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+    .box{
+        width:1000px;
+        margin:0 auto;
+        padding: 20px 0;
+        .left{
+            margin-right: 35px;
+            .menu{
+                position: relative;
+                ul{
+                    width:260px;
+                    border:1px solid #ccc;
+                    .menu-item{   
+                        display: flex;
+                        justify-content: space-between;
+                        padding:0 20px;
+                        box-sizing: border-box;
+                        height: 40px;
+                        line-height: 40px;
+                        border-bottom:1px solid #ccc;
+                        &:last-child{
+                            border-bottom: none;
+                        }
+                        .el-icon-arrow-right{
+                            line-height: 40px;
+                        }
+                    }
+                }
+                .recommend{
+                    position: absolute;
+                    top: 0px;
+                    left: 261px;
+                    display: none;
+                    z-index:-1;
+                    padding: 0 5px;
+                    width: 300px;
+                    border: 1px solid #ccc;
+                    background-color: #fff;
+                    .recommendItem{
+                        height: 40px;
+                        line-height: 40px;
+                        overflow: hidden;
+                        color:#ffa500;
+                        .ranking{
+                            margin-left: 10px;
+                            font-size: 20px;
+                            font-weight: 600;
+                            cursor:pointer;
+                        }
+                        .city{
+                            margin-left: 10px;
+                            font-size: 16px;
+                            cursor: pointer;
+                            &:hover{
+                                text-decoration: underline;
+                            }
+                        }
+                        .describe{
+                            margin-left: 10px;
+                            font-size: 14px;
+                            color:#ccc;
+                            cursor: pointer;
+                            &:hover{
+                                text-decoration: underline;
+                            }
+                        }
+                    }
+                }
+            }
+            .wishful{
+                margin-top: 20px;
+                width: 260px;
+                h4{
+                    margin-bottom: 10px;
+                    padding-bottom: 10px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ccc;
+                }
+                img{
+                    width:100%;
+                }
+            }
+        }
+        .right{
+            width:700px;
+            .search{
+                width:700px;
+                border:2px solid #ffa500;
+                input{
+                    padding:0 15px;
+                    width:610px;
+                    line-height: 36px;
+                    outline: none;
+                    border:none;
+                }
+                i{  
+                    margin-top: 8px;
+                    width: 40px;
+                    color:#ffa500;
+                    font-size: 24px;
+                    font-weight: 600;
+                    cursor: pointer
+                }
+            }
+            .searchRecommend{
+                margin: 10px 0;
+                font-size: 12px;
+                color:#333;
+                a{
+                    margin-left: 3px;
+                    cursor: pointer;
+                    &:hover{
+                        text-decoration: underline;
+                    }
+                }
+            }
+            .recommend-title{
+                padding: 10px 0;
+                width: 700px;
+                border-bottom: 1px solid #ccc;
+                h4{
+                    font-weight: 400;
+                    font-size: 18px;
+                    color:#ffa500;
+                    &::after{
+                        display: block;
+                        content: "";
+                        width:72px;
+                        height: 2px;
+                        background-color: #ffa500;
+                        position: absolute;
+                        bottom: 0;
+                        left: 0;
+                    }
+                }
+            }
+        }
+    }
+    
 </style>
